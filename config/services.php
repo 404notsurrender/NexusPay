0974eb6ebae45bbdd3429257f3c1929cb7d4c65c
404notsurrender <muhammad.zulfikar011@binus.ac.id>
2025-11-20 15:32:28 +0700

    added new array on config/services.php

--- config/services.php
+++ config/services.php
@@ -35,4 +35,10 @@ return [
         ],
     ],
 
+    'vip_reseller' => [
+        'api_id' => env('VIP_RESELLER_API_ID'),
+        'api_key' => env('VIP_RESELLER_API_KEY'),
+        'sign' => env('VIP_RESELLER_SIGN'),
+    ],
+
 ];
